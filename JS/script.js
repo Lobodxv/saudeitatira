47fc2fa0ed538967453dd573b84abdb793642271
lobodxv <manoellobo1@yahoo.com>
2024-09-16 11:02:37 -0300

    Reupdate

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === currentSlide) {
+            slide.classList.add('active');
+        }
+    });
+
+    document.querySelector('.carousel-inner').style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Inicializar o carrossel
+showSlide(currentSlide);
